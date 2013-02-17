60e5eeaf0338440f7e4221454b96a8c1e88c3fa2
miguel <miguel@ubuntu.(none)>
2013-02-17 17:41:22 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gabres::Application.config.secret_token = 'f14458e8b76ae4d2b27f702bafe94bca0ac38cfc732e8d5e178ce5e89388a3df274454302f77528c6b19b2e6a7eef0fa04df2bbc00c444630f84215c7c5080ea'
