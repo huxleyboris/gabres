52daf8cb6abb1646fed49b4fe3dc9d07102e1d97
Miguel Oliva <moliva@mulesoft.com>
2014-07-13 10:33:41 -0300

    Update 2014-07-13T13:33:41.218Z

--- app.js
+++ app.js
@@ -1,3 +1,239 @@
+(function() {
+	'use strict';
+
+	var initializeMainRoutes = function(routes, routeProvider) {
+		angular.forEach(routes, function(route, value) {
+			routeProvider.when(route.path, {
+				templateUrl: route.templateUrl,
+				controller: route.controller,
+				controllerAs: route.controllerAlias
+			});
+		});
+	};
+
+	angular.module('gabres', ['ngRoute', 'gabres-routes', 'gabres-main', 'gabres-directives', 'templates'])
+	  .config(function ($routeProvider, mainRoutes) {
+
+			initializeMainRoutes(mainRoutes, $routeProvider);
+
+	    $routeProvider
+	      .otherwise({
+	        redirectTo: '/'
+	      });
+	  });
+
+})();
+
+'app controller goes here';
+'common service goes here';
+(function() {
+  'use strict';
+
+  angular.module('gabres-directives', ['gabres-routes'])
+    .directive('menuBar', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/menu-bar.html',
+        controllerAs: 'menu',
+        controller: function(mainRoutes, $location) {
+          this.items = mainRoutes;
+
+          this.shouldShowSubitems = function(item) {
+            return item.subitems && item.subitems.length > 0;
+          };
+
+          this.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+          };
+        }
+      };
+    })
+    .directive('appFooter', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/app-footer.html'
+      };
+    })
+    .directive('product', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/product.html'
+      };
+    })
+    .directive('productLine', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/product-line.html'
+      };
+    })
+    .directive('headerBar', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/header-bar.html'
+      };
+    })
+    .directive('scrollSpy', function($window) {
+      return {
+        restrict: 'A',
+        controller: function($scope) {
+          $scope.spies = [];
+
+          this.addSpy = function(spyObj) {
+            $scope.spies.push(spyObj);
+          };
+        },
+        link: function(scope, elem, attrs) {
+          var findElem = function(elem, spy) {
+            var e = null;
+            angular.forEach(elem.find('product-line'), function(obj, value) {
+              if (spy.id === obj.id) {
+                e = obj;
+              }
+            });
+            return e;
+          };
+
+          var spyElems = [];
+
+          scope.$watch('spies', function(spies) {
+
+            angular.forEach(spies, function(spy, value) {
+              if (!spyElems[spy.id]) {
+                spyElems[spy.id] = findElem(elem, spy);
+              }
+            });
+          });
+
+          angular.element($window).bind('scroll', function() {
+            var highlightSpy = null;
+
+            // hack
+            if (scope.spies.length === 0) {
+              scope.$watch('spies', function(spies) {
+
+                angular.forEach(spies, function(spy, value) {
+                  if (!spyElems[spy.id]) {
+                    spyElems[spy.id] = findElem(elem, spy);
+                  }
+                });
+              });
+            }
+
+            angular.forEach(scope.spies, function(spy, value) {
+              spy.out();
+
+              spyElems[spy.id] = !spyElems[spy.id] ? findElem(elem, spy) : spyElems[spy.id];
+
+              if (spyElems[spy.id]) {
+                var pos = spyElems[spy.id].getBoundingClientRect().top;
+
+                if (pos - $window.scrollY <= 0) {
+                  spy.pos = pos;
+                  highlightSpy = highlightSpy ? highlightSpy : spy;
+                  if (highlightSpy.pos < spy.pos) {
+                    highlightSpy = spy;
+                  }
+                }
+              }
+            });
+
+            return highlightSpy !== null ? highlightSpy.in() : void 0;
+          });
+        }
+      };
+    })
+    .directive('spy', function($location) {
+      return {
+        restrict: 'A',
+        require: '^scrollSpy',
+        link: function(scope, elem, attrs, scrollSpy) {
+          elem.bind('click', function() {
+            scope.$apply(function() {
+              $location.hash(attrs.spy);
+            });
+          });
+
+          scrollSpy.addSpy({
+            id: attrs.spy,
+            in: function() {
+              elem.addClass('active');
+            },
+            out: function() {
+              elem.removeClass('active');
+            }
+          });
+        }
+      };
+    })
+    .directive('content', function($compile) {
+      var linker = function(scope, element, attrs) {
+        element.html(attrs.template).show();
+        $compile(element.contents())(scope);
+      };
+
+      return {
+          restrict: 'E',
+          replace: true,
+          link: linker,
+          scope: {
+              content:'='
+          }
+      };
+    })
+    .directive('map', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'assets/partials/map.html',
+        controller: function ($scope) {
+
+          var location = {
+            lat: 38.3193894,
+            long: -0.5553162,
+            city: 'Gabres S.L.',
+            desc: 'Plásticos Reforzados'
+          };
+
+          var mapOptions = {
+              zoom: 16,
+              center: new google.maps.LatLng(location.lat, location.long),
+              mapTypeId: google.maps.MapTypeId.HYBRID
+          };
+
+          $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+          $scope.markers = [];
+
+          var infoWindow = new google.maps.InfoWindow();
+
+          var createMarker = function (info) {
+
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(info.lat, info.long),
+                title: info.city
+            });
+
+            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+            google.maps.event.addListener(marker, 'click', function(){
+              infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+              infoWindow.open($scope.map, marker);
+            });
+
+            $scope.markers.push(marker);
+          };
+
+          createMarker(location);
+
+          $scope.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+          };
+        }
+      };
+    });
+})();
+
 (function(){
   'use strict';
 
@@ -214,5 +450,54 @@
         content: '<p class="text-justify">Chapas para techos, canalones, encofrados para la construcción, maceteros, cachas de motos de competición, cubas para la cria de tortugas o algas, etc. Además de pequeñas producciones en serie.</p><p class="text-justify">Todo en poliester y fibras de vidrio.</p><p class="text-center"><strong>¡Consúltenos!</strong></p>'
       };
     });
+
+})();
+
+(function() {
+  'use strict';
+
+  angular.module('gabres-routes', [])
+    .constant('mainRoutes', [
+      {
+        name:'Inicio',
+        path: '/',
+        templateUrl: 'main/main.html'
+      },
+      {
+        name: 'Productos',
+        path: '/products',
+        templateUrl: 'assets/partials/products.html',
+        controller: 'ProductLinesController',
+        controllerAlias: 'lineCtrl'
+        // subitems: [
+        //     {name:'Almacenamiento de Líquidos'},
+        //     {name:'Tratamiento de Aguas'},
+        //     {name:'Revestimientos'},
+        //     {name:'Línea Náutica'},
+        //     {name:'Trabajos Especiales'}
+        //   ]},
+      },
+      {
+        name:'Servicios',
+        path: '/services',
+        templateUrl: 'assets/partials/services.html'
+      },
+      {
+        name:'Galería',
+        path: '/gallery'
+      },
+      {
+        name:'Enlaces',
+        path: '/links',
+        templateUrl: 'assets/partials/links.html'
+      },
+      {
+        name: 'Contacto',
+        path: '/contact',
+        templateUrl: 'assets/partials/contact.html',
+        controller: 'ContactController',
+        controllerAlias: 'contactCtrl'
+      }
+    ]);
 
 })();
