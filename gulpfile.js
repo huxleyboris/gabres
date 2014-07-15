6110faeb7d7ba707bb8318ab63e1de959ce463bd
Miguel Oliva <moliva@mulesoft.com>
2014-07-15 15:29:16 -0300

    Added gulp deploy and changed fonts task directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,59 @@
 var gulp = require('gulp');
-var plugins = require("gulp-load-plugins")({lazy:false});
+var plugins = require('gulp-load-plugins')({lazy:false});
+var deploy = require('gulp-gh-pages');
 
 gulp.task('scripts', function(){
     //combine all js files of the app
     gulp.src(['!./app/**/*_test.js','./app/**/*.js'])
-        .pipe(plugins.jshint())
-        .pipe(plugins.jshint.reporter('default'))
-        .pipe(plugins.concat('app.js'))
-        .pipe(gulp.dest('./build'));
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('default'))
+      .pipe(plugins.concat('app.js'))
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('templates',function(){
     //combine all template files of the app into a js file
     gulp.src(['!./app/index.html',
-        './app/**/*.html'])
-        .pipe(plugins.angularTemplatecache('templates.js',{standalone:true}))
-        .pipe(gulp.dest('./build'));
+      './app/**/*.html'])
+      .pipe(plugins.angularTemplatecache('templates.js',{standalone:true}))
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('css', function(){
     gulp.src('./app/**/*.css')
-        .pipe(plugins.concat('app.css'))
-        .pipe(gulp.dest('./build'));
+      .pipe(plugins.concat('app.css'))
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('vendorJS', function(){
     //concatenate vendor JS files
     gulp.src(['!./bower_components/**/*.min.js',
-        './bower_components/**/*.js'])
-        .pipe(plugins.concat('lib.js'))
-        .pipe(gulp.dest('./build'));
+      './bower_components/**/*.js'])
+      .pipe(plugins.concat('lib.js'))
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('vendorCSS', function(){
     //concatenate vendor CSS files
     gulp.src(['!./bower_components/**/*.min.css',
-        './bower_components/**/*.css'])
-        .pipe(plugins.concat('lib.css'))
-        .pipe(gulp.dest('./build'));
+      './bower_components/**/*.css'])
+      .pipe(plugins.concat('lib.css'))
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('fonts', function(){
     gulp.src(['./bower_components/font-awesome/fonts/*', './bower_components/bootstrap/fonts/*'])
-        .pipe(gulp.dest('./build/fonts'));
+      .pipe(gulp.dest('./build/dummy/fonts'));
 });
 
 gulp.task('favicon', function(){
     gulp.src(['./app/favicon.ico'])
-        .pipe(gulp.dest('./build'));
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy-index', function() {
     gulp.src('./app/index.html')
-        .pipe(gulp.dest('./build'));
+      .pipe(gulp.dest('./build'));
 });
 
 gulp.task('watch',function(){
@@ -64,6 +65,7 @@ gulp.task('watch',function(){
         return gulp.src(event.path)
             .pipe(plugins.connect.reload());
     });
+
     gulp.watch(['./app/**/*.js','!./app/**/*test.js'],['scripts']);
     gulp.watch(['!./app/index.html','./app/**/*.html'],['templates']);
     gulp.watch('./app/**/*.css',['css']);
@@ -77,6 +79,11 @@ gulp.task('connect', function() {
     port: 9000,
     livereload: true
   });
+});
+
+gulp.task('deploy', function() {
+    gulp.src('./build/**/*')
+      .pipe(deploy({}));
 });
 
 gulp.task('default',['connect','scripts','templates','fonts', 'favicon','css','copy-index','vendorJS','vendorCSS','watch']);
