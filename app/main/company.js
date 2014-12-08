313d066684bcc19ae53c1c44fa7225c8c1f62cde
miguel <oliva.miguelh@gmail.com>
2014-12-08 22:50:15 +0100

    012: Retrying update

--- app/main/company.js
+++ app/main/company.js
@@ -5,7 +5,8 @@
     .constant('company', {
       name: 'Gabres S.L. - Plásticos Reforzados',
       emailAddress: 'info@prgabres.com',
-      phone: '965 105 963'
+      phone: '965 105 963',
+      address: ''
     });
 
 })();
