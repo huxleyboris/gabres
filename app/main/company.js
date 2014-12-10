78b71414243c463f93daa944230c68811dcb2502
miguel <oliva.miguelh@gmail.com>
2014-12-10 13:28:25 +0100

    Refactored app-footer and redesigned it

--- app/main/company.js
+++ app/main/company.js
@@ -4,6 +4,14 @@
   angular.module('gabres-company', [])
     .constant('company', {
       name: 'Gabres S.L. - Plásticos Reforzados',
+      physicalAddress: {
+        street: 'Finca La Ramona, 144 - Polígono "C"',
+        city: '(03114)  BACAROT - ALICANTE'
+      },
+      postalAddress: {
+        street: 'Apartado de Correo 5254',
+        city: '(03080)  ALICANTE'
+      },
       emailAddress: 'info@prgabres.com',
       phone: '965 105 963'
     });
